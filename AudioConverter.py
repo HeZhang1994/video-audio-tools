47ffb4bb795e48216cdd092b58ae6be57f38eefb
He Zhang <zhangheupc@126.com>
2019-02-14 11:46:17 +0000

    Update and rename AudioExtractor.py to AudioConverter.py

--- AudioConverter.py
+++ AudioConverter.py
@@ -1,10 +1,14 @@
+## Task description:
+## Convert audio formats.
+
 import subprocess
+
 
 # Input video/audio file: a.mp4
 # Output audio or path: b.wav or Audio/b.wav
 
-# The output format can be .wav or .mp3
-
 command = "ffmpeg -i a.mp3 -ab 160k -ac 2 -ar 44100 -vn b.wav"
 
 subprocess.call(command, shell=True)
+
+
