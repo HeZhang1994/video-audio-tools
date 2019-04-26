7978d0267e418ff50c04f849f7e6d76252d8c163
He Zhang <zhangheupc@126.com>
2019-04-26 14:48:56 +0100

    Update run_Video_01Clip.py

--- VideoAudio_Editing/run_Video_01Clip.py
+++ VideoAudio_Editing/run_Video_01Clip.py
@@ -51,7 +51,7 @@ def calc_time_diff(time1_str, time2_str):  # Not used in this code!
 
 # Part 1 Set the path of input and output media.
 
-INPUT_MEDIA = 'Media/JJY_Suddenly_PhantaCity.mp4'
+INPUT_MEDIA = 'Media/Demo_Suddenly_PhantaCity.mp4'
 # Download link: https://www.youtube.com/watch?v=Y36TtbFHm0M
 OUTPUT_PATH = 'Media/Clips_Video/'
 OUTPUT_NAME = 'Clip_'
