42e7e9e9b744ca78010d7f8602b941a9aea7f415
He Zhang <zhangheupc@126.com>
2019-04-27 02:40:56 +0100

    Update run_VAP_extract.py

--- VideoAudio_Processing/run_VAP_extract.py
+++ VideoAudio_Processing/run_VAP_extract.py
@@ -14,8 +14,8 @@ import subprocess
 
 
 # Set the path of input and output files.
-INPUT_VIDEO = "Media/Demo_HZY360P.mp4"
-OUTPUT_FILE = "Media/Demo_HZY.wav"
+INPUT_VIDEO = "Media/Demo_360P.mp4"
+OUTPUT_FILE = "Media/Demo.wav"
 
 # Check if the output file exists. If so, delete it.
 if os.path.isfile(OUTPUT_FILE) is True:
